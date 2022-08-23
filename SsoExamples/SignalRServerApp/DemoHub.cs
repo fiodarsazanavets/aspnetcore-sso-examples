fa7261f496dadc8058fe9bcaff3d54c3cd8f372c
Fiodar Sazanavets <f.sazanavets@gmail.com>
2022-08-23 15:43:00 +0100

    Add the initial code samples to the repo.

---
+++ SsoExamples/SignalRServerApp/DemoHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerApp;
+
+public class DemoHub : Hub
+{
+    public async Task BroadcastMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
